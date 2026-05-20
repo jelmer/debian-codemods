fecb52ff2220dff5b40bcee1a634cca50abf338e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-05-20 22:51:33 +0100

    Add fixer for description-contains-tabs.

--- lintian-brush/src/fixers/mod.rs
+++ lintian-brush/src/fixers/mod.rs
@@ -60,6 +60,7 @@ mod dep3_format_patch_author_or_from_is_better;
 mod dep5_file_paragraph_references_header_paragraph;
 mod depends_on_old_emacs;
 mod depends_on_python_minimal;
+mod description_contains_tabs;
 mod description_synopsis_starts_with_article;
 mod desktop_entry_contains_encoding_key;
 mod desktop_entry_file_has_crs;
